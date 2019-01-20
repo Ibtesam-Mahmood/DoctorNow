f8856d177d4942fa5b4eacf6c05179ada0d882ce
Subhanc <schaud86@uwo.ca>
2019-01-19 21:51:31 -0500

    react change

---
+++ src/components/Doctor.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Doctor extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App">
+        Your a doctor
+      </div>
+      </div>
+    );
+  }
+}
+
+export default Doctor;
---
+++ src/components/Nurse.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Nurse extends Component {
+  render() {
+    return (
+      <div className="App">
+        Your a nurse
+      </div>
+    );
+  }
+}
+
+export default Nurse;
